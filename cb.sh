023aa05f2eac28d822d1e8d24d39ebb09c3aa3de
nbtu <62731238+nbtu@users.noreply.github.com>
2024-08-15 15:19:55 +0800

    Update cb.sh

--- cb.sh
+++ cb.sh
@@ -19,7 +19,7 @@ for userId in ${userIds}; do
     http_code=`curl -sSL -w "%{http_code}" -o /dev/null --connect-timeout 5 --retry-delay 3 --retry 3 -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" https://jpeg.live.mmcdn.com/stream?room=${userId}`
     if [ "${http_code}" == 200 ]; then
         echo "开始获取直播源"
-        json=`curl --location 'https://chaturbate.com/get_edge_hls_url_ajax/' --header 'x-requested-with: XMLHttpRequest' --form 'room_slug="${userId}"'`
+        json=`curl --location 'https://chaturbate.com/get_edge_hls_url_ajax/' --header 'x-requested-with: XMLHttpRequest' --form "room_slug=${userId}"`
         #echo $(curl "https://chaturbate.com//streamapi/?modelname=${userId}")
         #json=`curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" "https://proxy.scrapeops.io/v1/?api_key=b3db67ba-385b-4f20-a1ea-4463df5ab939&url=https://chaturbate.com.tw/streamapi/?modelname=${userId}"` 
         #json=`curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" "https://chaturbate.com.tw/streamapi/?modelname=${userId}"`
@@ -35,7 +35,7 @@ for userId in ${userIds}; do
                 echo "The UID $uid exists in data.txt"
             else
                 echo "$userId 已推送到TG"
-                text="*J哥提醒你！！！！*\n\n#Chaturbate 主播 #${userId} 在线\n\n[让我康康！直播源地址](${m3u8site}?url=${hls})\n\n[直播间链接](https://www.chaturbate.com/${userId})\n\n_"
+                text="*J哥提醒你！！！！*\n\n#Chaturbate 主播 #${userId} 在线\n\n<a href='${m3u8site}?url=${hls}'>让我康康！直播源地址</a>\n\n<a href='https://www.chaturbate.com/${userId}'>直播间链接</a>\n\n_"
                 text=$(echo "${text}" | sed 's/-/\\\\-/g')
                 text=$(echo "${text}" | sed 's/_/\\\\_/g')
                 curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"@kbjol\", \"caption\":\"$text\", \"photo\":\"$img\"}" "https://api.telegram.org/${bot}/sendPhoto?parse_mode=MarkdownV2"
