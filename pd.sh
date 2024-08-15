5d60ed2f8811956cd36bba03948d24e9be3c394d
nbtu <62731238+nbtu@users.noreply.github.com>
2024-08-15 14:32:45 +0800

    Update pd.sh

--- pd.sh
+++ pd.sh
@@ -116,8 +116,7 @@ for userId in ${userIds[@]}; do
             echo "直播源：${hls}"
             
             echo "$userId 推送到TG"
-            #text="*J哥提醒你！！！！*\n\nPanda主播${userId}直播源已添加到SyncTV\n\n本场开播时间：$startTime（韩国时间快1小时）\n\n[直达地址，让我康康！](${synctv}/web/cinema/${roomid})\n\n[直达地址②，再次康康！](${m3u8site}?url=${userId})\n\n"
-            text="*J哥提醒你！！！！*\n\nPanda主播 #${userId}直播源已添加到SyncTV\n\n本场开播时间：$startTime（韩国时间快1小时）\n\n[直达地址](${m3u8site}?url=${userId})\n\n[直播间链接](https://www.pandalive.co.kr/live/play/${userId})\n\n"
+            text="*J哥提醒你！！！！*\n\nPanda主播 #${userId} 直播源已添加到SyncTV\n\n本场开播时间：$startTime（韩国时间快1小时）\n\n[直达地址](${m3u8site}?url=${userId})\n\n[直播间链接](https://www.pandalive.co.kr/live/play/${userId})\n\n"
             text=$(echo "${text}" | sed 's/-/\\\\-/g')
             curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"@kbjol\", \"caption\":\"$text\", \"photo\":\"$img\"}" "https://api.telegram.org/${bot}/sendPhoto?parse_mode=MarkdownV2"                echo -e "$userId $roomid $roomToken $hls">> data.txt
             echo -e "添加$userId $hls">> $logfile
