5f9de93589f744f98b41fe9a93ff62e5cf92b657
nbtu <62731238+nbtu@users.noreply.github.com>
2024-08-15 15:07:00 +0800

    Update pd.sh

--- pd.sh
+++ pd.sh
@@ -118,7 +118,8 @@ for userId in ${userIds[@]}; do
             echo "$userId 推送到TG"
             text="*J哥提醒你！！！！*\n\n#Panda 主播 #${userId} 直播源已添加到SyncTV\n\n本场开播时间：$startTime（韩国时间快1小时）\n\n[直达地址](${m3u8site}?url=${userId})\n\n[直播间链接](https://www.pandalive.co.kr/live/play/${userId})\n\n"
             text=$(echo "${text}" | sed 's/-/\\\\-/g')
-            curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"@kbjol\", \"caption\":\"$text\", \"photo\":\"$img\"}" "https://api.telegram.org/${bot}/sendPhoto?parse_mode=MarkdownV2"                echo -e "$userId $roomid $roomToken $hls">> data.txt
+            curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"@kbjol\", \"caption\":\"$text\", \"photo\":\"$img\"}" "https://api.telegram.org/${bot}/sendPhoto?parse_mode=MarkdownV2"                
+            echo -e "$userId $roomid $roomToken $hls">> data.txt
             echo -e "添加$userId $hls">> $logfile
         else 
             echo "$userId 获取直播源失败！"
