4ba4fe0c1e13c0bd836f56b97b0e59906f056047
nbtu <62731238+nbtu@users.noreply.github.com>
2024-08-15 22:49:50 +0800

    Update flex.sh

--- flex.sh
+++ flex.sh
@@ -16,12 +16,13 @@ echo -e `date` >> $logfile
 
 
 for userId in ${userIds}; do
-    if grep -q "${userId}" data.txt; then
-        echo "The UID $uid exists in data.txt"
-    else
-        channelId=$(curl  -s "https://api.flextv.co.kr/api/lives/search?limit=1&name=${userId}" |jq -r .data[0].channelId)
-        if [ -n "$channelId" ] && [ "$channelId" != null ] ; then
-            echo "开始获取直播源"
+    channelId=$(curl  -s "https://api.flextv.co.kr/api/lives/search?limit=1&name=${userId}" |jq -r .data[0].channelId)
+    if [ -n "$channelId" ] && [ "$channelId" != null ] ; then
+        echo "在线，开始获取直播源"
+        echo -e "$userId ">> online.txt
+        if grep -q "${userId}" data.txt; then
+            echo "The UID $uid exists in data.txt"
+        else
             res=`curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" -H 'x-device-info:{"t":"webMobile","v":"1.0","ui":24631221}' -H "cookie:${FLEXCOOKIE}"  "https://api.flextv.co.kr/api/channels/${channelId}/stream?option=all" `
             hls=$(echo "$res"| jq -r .sources[].url)
             userId=$(echo "$res"| jq -r .owner.loginId)
@@ -41,11 +42,11 @@ for userId in ${userIds}; do
             else 
             	echo "$userId 获取直播源失败！"
             	echo "错误提示： $res"
-            fi  
-        else
-            echo "$userId 可能没开播。"
+            fi 
         fi
-        echo "-----------`date`--------------"
-        sleep 2
+    else
+        echo "$userId 可能没开播。"
     fi
+    echo "-----------`date`--------------"
+    sleep 1
 done
