91288921d9d2b229bbafa29d2f3b432ed0af4915
nbtu <62731238+nbtu@users.noreply.github.com>
2024-08-15 16:37:25 +0800

    Update flex.sh

--- flex.sh
+++ flex.sh
@@ -23,7 +23,7 @@ for userId in ${userIds}; do
         if [ -n "$channelId" ] && [ "$channelId" != null ] ; then
 			echo "开始获取直播源"
 			res=`curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" -H 'x-device-info:{"t":"webMobile","v":"1.0","ui":24631221}' -H "cookie:${FLEXCOOKIE}"  "https://api.flextv.co.kr/api/channels/${channelId}/stream?option=all" `
-        	url=$(echo "$res"| jq -r .sources[].url)
+        	hls=$(echo "$res"| jq -r .sources[].url)
         	userId=$(echo "$res"| jq -r .owner.loginId)
         	img=$(echo "$res"| jq -r .thumbUrl)
         	startTime=$(echo "$res"| jq -r .stream.createdAt)
@@ -32,7 +32,7 @@ for userId in ${userIds}; do
             	echo "直播源：${hls}"
 
             	echo "$userId 推送到TG"
-            	text="<b>J哥提醒你！！！！</b>\n\n#Flextv 主播 #${userId} 在线\n\n本场开播时间：$startTime（UTC时间+8小时）\n\n<a href='${hls}'>直播源地址</a>\n\n<a href='https://www.flextv.co.kr/channels/${channelId}/live'>直播间链接</a>\n\n-----"
+            	text="<b>J哥提醒你！！！！</b>\n\n#Flextv 主播 #${userId} 在线\n\n本场开播时间：$startTime（UTC时间+8小时）\n\n<a href='{m3u8site}?url=${hls}'>直播源地址</a>\n\n<a href='https://www.flextv.co.kr/channels/${channelId}/live'>直播间链接</a>\n\n-----"
             	#text=$(echo "${text}" | sed 's/-/\\\\-/g')
             	curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"@kbjol\", \"caption\":\"$text\", \"photo\":\"$img\"}" "https://api.telegram.org/${bot}/sendPhoto?parse_mode=HTML"
             	echo -e "$userId $hls">> data.txt
