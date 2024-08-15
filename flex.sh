599a4bc80cebaafc7458b9bf1c8d83022ca53319
nbtu <62731238+nbtu@users.noreply.github.com>
2024-08-15 16:49:32 +0800

    Update flex.sh

--- flex.sh
+++ flex.sh
@@ -21,13 +21,13 @@ for userId in ${userIds}; do
     else
         channelId=$(curl  -s "https://api.flextv.co.kr/api/lives/search?limit=1&name=${userId}" |jq -r .data[0].channelId)
         if [ -n "$channelId" ] && [ "$channelId" != null ] ; then
-			echo "开始获取直播源"
-			res=`curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" -H 'x-device-info:{"t":"webMobile","v":"1.0","ui":24631221}' -H "cookie:${FLEXCOOKIE}"  "https://api.flextv.co.kr/api/channels/${channelId}/stream?option=all" `
-        	hls=$(echo "$res"| jq -r .sources[].url)
-        	userId=$(echo "$res"| jq -r .owner.loginId)
-        	img=$(echo "$res"| jq -r .thumbUrl)
-        	startTime=$(echo "$res"| jq -r .stream.createdAt)
-        	if [ -n "$url" ] && [ "$url" != null ] ; then
+            echo "开始获取直播源"
+            res=`curl -sSL --connect-timeout 5 --retry-delay 3 --retry 3 -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36" -H 'x-device-info:{"t":"webMobile","v":"1.0","ui":24631221}' -H "cookie:${FLEXCOOKIE}"  "https://api.flextv.co.kr/api/channels/${channelId}/stream?option=all" `
+            hls=$(echo "$res"| jq -r .sources[].url)
+            userId=$(echo "$res"| jq -r .owner.loginId)
+            img=$(echo "$res"| jq -r .thumbUrl)
+            startTime=$(echo "$res"| jq -r .stream.createdAt)
+            if [ -n "$hls" ] && [ "$hls" != null ] ; then
             	echo "${userId}获取成功。"
             	echo "直播源：${hls}"
 
@@ -37,14 +37,14 @@ for userId in ${userIds}; do
             	curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"@kbjol\", \"caption\":\"$text\", \"photo\":\"$img\"}" "https://api.telegram.org/${bot}/sendPhoto?parse_mode=HTML"
             	echo -e "$userId $hls">> data.txt
             	echo -e "添加$userId $hls">> $logfile
-        	else 
+            else 
             	echo "$userId 获取直播源失败！"
-            	echo "错误提示： $res "
-        	fi  
-		else
-  			echo "$userId 可能没开播。"
-	 	fi
+            	echo "错误提示： $res"
+            fi  
+        else
+            echo "$userId 可能没开播。"
+        fi
         echo "-----------`date`--------------"
         sleep 2
     fi
-done   
+done
