5acd0a19da50d99d23ce5ae847e6c908369efefe
nbtu <62731238+nbtu@users.noreply.github.com>
2024-08-15 16:11:11 +0800

    Update cb.sh

--- cb.sh
+++ cb.sh
@@ -35,7 +35,8 @@ for userId in ${userIds}; do
                 echo "The UID $uid exists in data.txt"
             else
                 echo "$userId 已推送到TG"
-                text="*J哥提醒你！！！！*\n\n#Chaturbate 主播 #${userId} 在线\n\n<a href='${m3u8site}?url=${hls}'>让我康康！直播源地址</a>\n\n<a href='https://www.chaturbate.com/${userId}'>直播间链接</a>\n\n_"
+                text="J哥提醒你！！！！\n\n#Chaturbate 主播 #${userId} 在线\n\n<a href='${m3u8site}?url=${hls}'>让我康康！直播源地址</a>\n\n<a href='https://www.chaturbate.com/${userId}'>直播间链接</a>\n\n_"
+                echo $text
                 #text=$(echo "${text}" | sed 's/-/\\\\-/g')
                 #text=$(echo "${text}" | sed 's/_/\\\\_/g')
                 curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"@kbjol\", \"caption\":\"$text\", \"photo\":\"$img\"}" "https://api.telegram.org/${bot}/sendPhoto?parse_mode=HTML"
